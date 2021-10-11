a6582da720c736068627c95236ab7a8e8f26a132
Jonathan Shine <jonathanhshine@gmail.com>
2021-10-11 13:59:57 +0200

    added inital endpoint, server listener, and erro handling to server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // IMPORTS ------------------------------------------
 import express from 'express';
+import cors from 'cors';
 import "./db-connect.js";
 // --------------------------------------------------
 
@@ -9,3 +10,33 @@ const app = express();
 // --------------------------------------------------
 
 
+// MIDDLEWARE ---------------------------------------
+app.use( express.json() );
+
+app.use( cors() );
+// --------------------------------------------------
+
+
+// ROUTES -------------------------------------------
+app.get("/", (req, res) => {
+    res.send(`<h1>aChord Backend</h1>`)
+});
+// --------------------------------------------------
+
+
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`App listening at http://localhost:.${PORT}`);
+});
+
+
+app.use(
+    function errorHandler (err, req, res, next) {
+        res.status(err.status || 400).send({
+            error: {
+                message: err.message,
+                status: err.status
+            }
+        });
+    }
+);
