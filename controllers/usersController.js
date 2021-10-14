a2c6c2421e8f50342fa633e7d5c236d583223d21
Tim Jungmann <tim-j11@web.de>
2021-10-14 12:15:34 +0200

    implement user authentication by cookie to stay logged in

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -25,6 +25,10 @@ export const getOneUser = async (req, res, next) => {
     };
 };
 
+export const verifyCookie = (req, res) => {
+  res.send(req.user);
+};
+
 export const createUser = async (req, res, next) => {
     const data = req.body;
     
